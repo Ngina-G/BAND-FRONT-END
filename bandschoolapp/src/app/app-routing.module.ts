42dd134ed04f777a8e1db7cb9c3a88f2bdab4552
Ngina <Nginagitau11@outlook.com>
2022-07-04 10:35:24 +0300

    update: adds logo to pomodoro component

--- bandschoolapp/src/app/app-routing.module.ts
+++ bandschoolapp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home/home.component';
+import { PomodoroComponent } from './modules/home/pages/pomodoro/pomodoro/pomodoro.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
+  {path: 'pomodoro', component: PomodoroComponent},
 ];
 
 @NgModule({
--- bandschoolapp/src/app/app.module.ts
+++ bandschoolapp/src/app/app.module.ts
@@ -11,16 +11,18 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PomodoroComponent } from './modules/home/pages/pomodoro/pomodoro/pomodoro.component';
-import { HomeComponent } from './modules/home/home/home.component'
+import { HomeComponent } from './modules/home/home/home.component';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PomodoroComponent,
     HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
