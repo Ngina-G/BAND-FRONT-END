364cb2491f6bfd1d1ff5ae46a6dd89bccf22b40f
Ngina <Nginagitau11@outlook.com>
2022-07-05 20:08:32 +0300

    fix: routing error

--- bandschoolapp/src/app/app-routing.module.ts
+++ bandschoolapp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home/home.component';
 import { PomodoroComponent } from './modules/home/pages/pomodoro/pomodoro/pomodoro.component';
+import { AllNotesComponent } from './modules/home/pages/all-notes/all-notes.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
   {path: 'pomodoro', component: PomodoroComponent},
+  {path: 'all-notes', component: AllNotesComponent},
   
 ];
 
--- bandschoolapp/src/app/modules/home/home/home.component.ts
+++ bandschoolapp/src/app/modules/home/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title: string = 'The title';
+  title: string = 'BAND';
+  show = false;
   constructor() {}
 
   ngOnInit() {}
