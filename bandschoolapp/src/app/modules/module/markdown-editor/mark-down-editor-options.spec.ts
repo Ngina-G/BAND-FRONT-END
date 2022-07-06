99b3d13d9da48095d36d99367e014cafd94c85c1
Ngina <Nginagitau11@outlook.com>
2022-07-06 15:19:43 +0300

    feat: fix err

--- bandschoolapp/src/app/modules/module/markdown-editor/mark-down-editor-options.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MarkdownEditorOptions } from "./mark-down-editor-options";
-
-describe('MarkDownEditorOptions', () => {
-  it('should create an instance', () => {
-    expect(new MarkdownEditorOptions()).toBeTruthy();
-  });
-});
--- bandschoolapp/src/app/modules/module/markdown-editor/markdown-editor.module.ts
+++ bandschoolapp/src/app/modules/module/markdown-editor/markdown-editor.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EditorComponent } from './editor/editor.component';
 
-import { MarkdownEditorOptions } from './mark-down-editor-options';
 import { MarkDownEditorComponent } from './mark-down-editor/mark-down-editor.component';
 @NgModule({
   declarations: [
-    EditorComponent,
     MarkDownEditorComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    EditorComponent
   ]
 })
 export class MarkdownEditorModule { }
