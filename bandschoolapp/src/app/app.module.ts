8dc5d665104e09260bfcce1871a6307334ed85a8
Ngina <Nginagitau11@outlook.com>
2022-07-13 12:18:14 +0300

    feat: adds blaise notes

--- bandschoolapp/src/app/app.module.ts
+++ bandschoolapp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
